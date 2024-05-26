aa86e6abf42390e4a1b988c75183d921f1c10f5e
jack <jackliang1987.com>
2024-05-26 11:29:03 +0800

    removed unused useRef in theme.js

--- src/providers/theme.js
+++ src/providers/theme.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect, useRef } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import usePrevious from "../hooks/usePrevious";
 
